b946126ed50fc0c56dfdf4adc7f09ba7b90111bf
alukacher <alukacher@brown.edu>
2023-03-10 11:39:44 -0500

    Fix error with excel_upload. Now works correctly. Avi = 1 hour.

--- excel_upload.m
+++ excel_upload.m
@@ -2,7 +2,19 @@ function vicon_matrix = excel_upload(vicon_and_delsys_data)
 
 vicon_and_delsys_matrix = readmatrix(vicon_and_delsys_data);
 
-ref_point = (vicon_and_delsys_matrix == 69420); %finds point in matrix between vicon and delsys data sets
+%finds point in matrix between vicon and delsys data sets
+%the point where one index == 100, and the next in that row is NaN
+%specifically appears right before the vicon data. This point becomes the
+%reference point.
+size_data_sheet = size(vicon_and_delsys_matrix);
+for row = 1:size_data_sheet(1)
+    for col = 1:size_data_sheet(2)
+        if vicon_and_delsys_matrix(row,col) == 100 && isnan(vicon_and_delsys_matrix(row,col+1))
+            ref_point = row;
+            break
+        end
+    end
+end
 
-vicon_matrix = vicon_and_delsys_matrix((ref_point(1)+2):end, 3:67); %vicon corresponds to coordinate (xyz) data
+vicon_matrix = vicon_and_delsys_matrix((ref_point+4):end, 3:end); %vicon corresponds to coordinate (xyz) data
 end
