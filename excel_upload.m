c2875175d4b000fc8933c28efaf0c9c019179d09
alukacher <alukacher@brown.edu>
2023-03-10 10:33:09 -0500

    Update excel_upload.m
    Update excel_upload with possible fix. Avi 0.5 hours

--- excel_upload.m
+++ excel_upload.m
@@ -2,7 +2,7 @@ function vicon_matrix = excel_upload(vicon_and_delsys_data)
 
 vicon_and_delsys_matrix = readmatrix(vicon_and_delsys_data);
 
-ref_point = (vicon_and_delsys_matrix == "Frame"); %finds point in matrix between vicon and delsys data sets
+ref_point = (vicon_and_delsys_matrix == 69420); %finds point in matrix between vicon and delsys data sets
 
-vicon_matrix = vicon_and_delsys_matrix((ref_point+2):end, 3:67); %vicon corresponds to coordinate (xyz) data
+vicon_matrix = vicon_and_delsys_matrix((ref_point(1)+2):end, 3:67); %vicon corresponds to coordinate (xyz) data
 end
