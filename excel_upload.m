3b065dbc205d0bce2ebe9af5891fd4bde4c7e117
alukacher <alukacher@brown.edu>
2023-03-10 11:47:03 -0500

    Update excel_upload.m

--- excel_upload.m
+++ excel_upload.m
@@ -7,6 +7,7 @@ vicon_and_delsys_matrix = readmatrix(vicon_and_delsys_data);
 %specifically appears right before the vicon data. This point becomes the
 %reference point.
 size_data_sheet = size(vicon_and_delsys_matrix);
+
 for row = 1:size_data_sheet(1)
     for col = 1:size_data_sheet(2)
         if vicon_and_delsys_matrix(row,col) == 100 && isnan(vicon_and_delsys_matrix(row,col+1))
