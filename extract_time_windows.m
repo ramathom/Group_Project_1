12cf3959cb70ab495d2d48340c9648b3559756bb
alukacher <alukacher@brown.edu>
2023-03-12 15:46:45 -0400

    Update extract_time_windows.m

--- extract_time_windows.m
+++ extract_time_windows.m
@@ -5,7 +5,7 @@ time_windows = [];
 
 %checks that the coordinates at each frame are complete (contains numbers
 %at x, y, and z). If true, then that frame is added to time_windows. All
-%incomplete coordinates (contain NaN) are excluded.
+%incomplete coordinates (contain NaN at X,Y,and/or Z) are excluded.
 for frame = 1:num_of_frames(1)
     if ~isnan(marker_of_interest(frame,1)) && ~isnan(marker_of_interest(frame,2)) && ~isnan(marker_of_interest(frame,3))
         time_windows = [time_windows frame];
