f474edcf08cec1afaa776919a12731e0fa183e61
alukacher <alukacher@brown.edu>
2023-03-13 10:31:34 -0400

    Avi 2 hours
    Made some edits, Avi 2 hours

--- extract_time_windows.m
+++ extract_time_windows.m
@@ -3,11 +3,10 @@ function time_windows = extract_time_windows(marker_of_interest)
 num_of_frames = size(marker_of_interest);
 time_windows = [];
 
-%checks that the coordinates at each frame are complete (contains numbers
-%at x, y, and z). If true, then that frame is added to time_windows. All
-%incomplete coordinates (contain NaN at X,Y,and/or Z) are excluded.
+%checks that the coordinates at each frame are incomplete (contains NaN
+%at x, y, or z). If true, then that frame is added to time_windows.
 for frame = 1:num_of_frames(1)
-    if ~isnan(marker_of_interest(frame,1)) && ~isnan(marker_of_interest(frame,2)) && ~isnan(marker_of_interest(frame,3))
+    if isnan(marker_of_interest(frame,1)) || isnan(marker_of_interest(frame,2)) || isnan(marker_of_interest(frame,3))
         time_windows = [time_windows frame];
     end
 end
