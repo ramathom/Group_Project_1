7fd23f5b6ea14e83eaa73a39bad9e3957a361265
nataliechernysh <natalie_chernysh@brown.edu>
2023-03-14 16:06:26 -0400

    Update marker_graph.m

--- marker_graph.m
+++ marker_graph.m
@@ -1,7 +1,7 @@
 function Three_dimensional_plot = marker_graph(vicon_and_delsys_data) 
 %the input should be the specific marker outputted from the marker of interest function
   [marker_of_interest, name_of_marker] = extract_marker_of_interest(vicon_and_delsys_data); 
-  time_windows = extract_time_windows(marker_of_interest);
+
   %if columns are incomplete, call the time windows in marker of interest
  
   X= marker_of_interest(:,1);%select first column 
@@ -14,7 +14,7 @@ function Three_dimensional_plot = marker_graph(vicon_and_delsys_data)
   zlabel('Z Data (mm)')
   
   title('Vicon System: 3D Visualization of', name_of_marker)  
-  % title specific to whichever marker is being
+  % title specific to whichever marker is called 
   
 end
 
