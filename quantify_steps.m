262bfd5493a57b3460049127d72c56111d117a43
alukacher <alukacher@brown.edu>
2023-03-14 16:08:51 -0400

    Avi, 2 hours
    Fixes to extract time windows and quantify steps

--- quantify_steps.m
+++ quantify_steps.m
@@ -12,26 +12,28 @@ left_foot_steps = 0;
 %position.
 
 %quantifies right steps.
-for index = 1:length(RANK_time_windows)-1
+for index = 5:length(RANK_time_windows)-5
 
     current_frame = RANK_time_windows(index);
-    next_frame = RANK_time_windows(index+1);
+    next_frame = RANK_time_windows(index+4);
+    previous_frame = RANK_time_windows(index-4);
 
-    if RANK_time_windows(index+1) == RANK_time_windows(index) + 1 %only considering frames that are consecutive
-        if RANK(current_frame,3) > RANK(next_frame,3) %if the current frame z coordinate is greater than the next frame, it's likely a maxima.
+    if RANK_time_windows(index+4) == RANK_time_windows(index) + 4 && RANK_time_windows(index-4) == RANK_time_windows(index) - 4 %only considering frames that are consecutive
+        if RANK(current_frame,3) > RANK(next_frame,3) && RANK(current_frame,3) > RANK(previous_frame,3) %if the current frame z coordinate is greater than the next frame, it's likely a maxima.
             right_foot_steps = right_foot_steps + 1;
         end
     end
 end
 
 %quantifies left steps.
-for index = 1:length(LANK_time_windows)-1
+for index = 5:length(LANK_time_windows)-5
 
     current_frame = LANK_time_windows(index);
-    next_frame = LANK_time_windows(index+1);
+    next_frame = LANK_time_windows(index+4);
+    previous_frame = LANK_time_windows(index-4);
 
-    if LANK_time_windows(index+1) == LANK_time_windows(index) + 1
-        if LANK(current_frame) > LANK(next_frame)
+    if LANK_time_windows(index+4) == LANK_time_windows(index) + 4 && LANK_time_windows(index-4) == LANK_time_windows(index) - 4
+        if LANK(current_frame) > LANK(next_frame) && LANK(current_frame,3) > LANK(previous_frame,3)
             left_foot_steps = left_foot_steps + 1;
         end
     else
