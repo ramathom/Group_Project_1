943986d26eaa1e14056bbda38012e93b0b685cf2
nataliechernysh <natalie_chernysh@brown.edu>
2023-03-14 16:06:18 -0400

    Update extract_time_windows.m

--- extract_time_windows.m
+++ extract_time_windows.m
@@ -5,7 +5,7 @@ num_of_frames = size(marker_of_interest);
 time_windows = [];
 
 %checks that the coordinates at each frame are incomplete (contains NaN
-%at x, y, or z). If true, then that frame is added to time_windows.
+%at x, y, or z). If incomplete, then that frame is added to time_windows.
 for frame = 1:num_of_frames(1)
     if isnan(marker_of_interest(frame,1)) || isnan(marker_of_interest(frame,2)) || isnan(marker_of_interest(frame,3))
         time_windows = [time_windows frame]; %updating matrix to include each subsequent frame that's NaN 
