f9d59f9854437e74f65f5a9785edbd4ca0c326f6
nataliechernysh <natalie_chernysh@brown.edu>
2023-03-14 16:04:56 -0400

    Update gaps_in_data.m
    Starting to de-bug (rachel and Nat)-.5 hours

--- gaps_in_data.m
+++ gaps_in_data.m
@@ -6,11 +6,11 @@ gaps = '';
 num_of_gaps = 0;
 
 for count = 1:length(list_of_markers)
-    [time_window_of_marker,name_of_marker] = extract_time_windows(list_of_markers(count));
-    if length(time_window_of_marker) >= 1
+    [time_marker] = extract_time_windows(list_of_markers(count));
+    if length(time_marker) >= 1
         data_matrix(1,count) = name_of_marker;
-        for n = 1:length(time_window_of_marker)-1
-            if time_window_of_marker(n) + 1 ~= time_window_of_marker(n+1)
+        for n = 1:length(time_marker)-1
+            if time_marker(n) + 1 ~= time_marker(n+1)
                 num_of_gaps = num_of_gaps + 1;
             end
         end
