7bb87ffa9b2e4955f0b73abc10d9649a1faf6d71
alukacher <alukacher@brown.edu>
2023-03-20 13:25:07 -0400

    Avi 0.5 hour
    Fixed bug in master function so that input continues cycling if user prompt is invalid.

--- master_function.m
+++ master_function.m
@@ -7,12 +7,17 @@ vicon_dimensions = size(vicon_data);
 dimensions_string = strcat("The vicon data is"," ", num2str(vicon_dimensions(1)),"X",num2str(vicon_dimensions(2))," ","large.");
 disp(dimensions_string);
 
+%displays all of the markers available to analyze.
+disp("These are the available markers: [LFHD, RFHD, LBDH, RBHD, C7, T10, CLAV, STRN, RBAK, LSHO, LUPA, LELB, LFRM, LWRA, LWRB, LFIN, RSHO, RUPA, RELB, RFRM, RWRA, RWRB, RFIN, LASI, RASI, LPSI, RPSI, LTHI, LKNE, LTIB, LANK, LHEE, LTOE, RTHI, RKNE, RTIB, RANK, RHEE, RTOE]");
+
 %asks if user wants to analyze a specific marker. This prompt continues
 %cycling if the user input is invalid.
 invalid_prompt = true;
 prompt = "Do you want to analyze a specific marker? Type y for yes or n for no: ";
 txt_yes_or_no = input(prompt,'s');
 
+%checks that the user prompt is valid. If invalid, continues cycling with
+%the same prompt until a valid answer is given.
 while invalid_prompt
 
     if txt_yes_or_no == 'y' || 'n'
@@ -35,19 +40,23 @@ end
 %if the user wants to analyze a specific marker, executes the following
 %code. cycle_again is true at first, but the entire loop will continue
 %executing if the user wants to analyze another marker.
-cycle_again = true;
+if boolean_yes
+    cycle_again = true;
+else
+    cycle_again = false;
+end
 
 while cycle_again
-    if boolean_yes
-        
-        %scrolls through list of markers and checks if user input matches any of
-        %the items. User input must exactly match one of the items
-        %(case-sensitive), or else the prompt is repeated. Once the marker is
-        %identified, the index is stored in the variable 'marker.'
-        prompt = "Which marker do you want to analyze? ";
-        txt = input(prompt,'s');
-        invalid_prompt = true;
-        list_of_markers = ["LFHD","RFHD","LBDH","RBHD","C7","T10","CLAV","STRN","RBAK","LSHO","LUPA","LELB","LFRM","LWRA","LWRB","LFIN","RSHO","RUPA","RELB","RFRM","RWRA","RWRB","RFIN","LASI","RASI","LPSI","RPSI","LTHI","LKNE","LTIB","LANK","LHEE","LTOE","RTHI","RKNE","RTIB","RANK","RHEE","RTOE"];
+    %scrolls through list of markers and checks if user input matches any of
+    %the items. User input must exactly match one of the items
+    %(case-sensitive), or else the prompt is repeated. Once the marker is
+    %identified, the index is stored in the variable 'marker.'
+    prompt = "Which marker do you want to analyze? ";
+    txt = input(prompt,'s');
+    invalid_prompt = true;
+    list_of_markers = ["LFHD","RFHD","LBDH","RBHD","C7","T10","CLAV","STRN","RBAK","LSHO","LUPA","LELB","LFRM","LWRA","LWRB","LFIN","RSHO","RUPA","RELB","RFRM","RWRA","RWRB","RFIN","LASI","RASI","LPSI","RPSI","LTHI","LKNE","LTIB","LANK","LHEE","LTOE","RTHI","RKNE","RTIB","RANK","RHEE","RTOE"];
+    
+    while invalid_prompt
         if ismember(txt,list_of_markers)
             invalid_prompt = false;
             marker = find(list_of_markers == txt);
@@ -55,21 +64,20 @@ while cycle_again
             prompt = "Invalid input. Which marker do you want to analyze? ";
             txt = input(prompt,'s');
         end
-        
-        %indicates if there are any gaps (NaN coordinates) in the data for a
-        %specific marker.
-        gaps_present = gaps_in_data(vicon_and_delsys_data,txt)
-    
-        %presents three graphs: coordinates, coordinates over time, and
-        %elevation (z coordinate) over time.
-        subplot(1,3,1);
-            marker_graph(vicon_and_delsys_data,txt);
-        subplot(1,3,2);
-            fourd_graph(vicon_and_delsys_data,txt);
-        subplot(1,3,3);
-            elevation_graph(vicon_and_delsys_data,txt);
-    
     end
+        
+    %indicates if there are any gaps (NaN coordinates) in the data for a
+    %specific marker.
+    gaps_present = gaps_in_data(vicon_and_delsys_data,txt)
+    
+    %presents three graphs: coordinates, coordinates over time, and
+    %elevation (z coordinate) over time.
+    subplot(1,3,1);
+        marker_graph(vicon_and_delsys_data,txt);
+    subplot(1,3,2);
+        fourd_graph(vicon_and_delsys_data,txt);
+    subplot(1,3,3);
+        elevation_graph(vicon_and_delsys_data,txt);
     
    %Asks if user wants to analyze another marker. If yes, cycles again
    %through this loop.
